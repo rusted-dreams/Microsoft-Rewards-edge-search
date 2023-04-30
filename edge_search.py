257b2627f8620e035056bcc6d208f7e772a076a3
rusted-dreams <123829783+rusted-dreams@users.noreply.github.com>
2023-04-30 22:27:43 +0530

    add more comments to edge_search.py

--- edge_search.py
+++ edge_search.py
@@ -29,12 +29,12 @@ def perform_search(): #This opens a new tab and does a random word search.
     open_edge_browser()
     
     for i in range(10):
-        search_term = RandomWord().word()
-        time.sleep(2)
+        search_term = RandomWord().word() #generates a random word to search for.
+        time.sleep(2)  #waits 2 sec before opening a new tab to allow the search to happen first.
         url = f"https://www.bing.com/search?q={search_term}"
         webbrowser.get("edge").open_new_tab(url)
 
-    os.system("taskkill /f /im msedge.exe")
+    os.system("taskkill /f /im msedge.exe") #Closes the browser for after 10 searches/tabs.
 
 
 for i in range(9):
